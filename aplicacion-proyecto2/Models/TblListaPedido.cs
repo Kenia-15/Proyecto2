26555eba0ea5ee4e610a1273ab211ed2bb4a358e
Kenia <kenia.aguero@uned.cr>
2024-11-28 21:15:50 -0600

    Inicio lógica pedidos

---
+++ aplicacion-proyecto2/Models/TblListaPedido.cs
@@ -0,0 +1,13 @@
+﻿namespace aplicacion_proyecto2.Models
+{
+    public class TblListaPedido
+    {
+        public TblPedido? pedido { get; set; }
+        public TblDetallePedido? detallePedido { get; set; }
+        public TblUsuario? usuario { get; set; }
+        public TblProvincia? provincia { get; set; }
+        public TblCantone? cantone { get; set; }
+        public TblDistrito? distrito { get; set; } 
+
+    }
+}
